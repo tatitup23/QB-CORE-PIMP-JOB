aae3582534d5adfa77f8161cd661f2f914f25a92
TatitupTech <115635661+tatitup23@users.noreply.github.com>
2025-05-26 09:40:27 -0400

    Add files via upload

---
+++ PIMPJOB.sql
@@ -0,0 +1,6 @@
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+('pimp', 'Pimp', 0);
+
+INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+('pimp', 0, 'pimp', 'Pimp', 200, '{}', '{}'),
+('pimp', 1, 'boss', 'Boss', 400, '{}', '{}');
